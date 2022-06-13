36d531657eb3dc1aa73d9d01c550303f0ad4a85b
h0225w <gusdn5387@naver.com>
2022-06-14 00:09:41 +0900

    카카오 로그인 테스트

--- iOSMovieApp/ViewController.swift
+++ iOSMovieApp/ViewController.swift
@@ -6,14 +6,30 @@
 //
 
 import UIKit
+import KakaoSDKUser
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func kakaoLoginTest(_ sender: Any) {
+        if (UserApi.isKakaoTalkLoginAvailable()) {
+            UserApi.shared.loginWithKakaoTalk {(oauthToken, error) in
+                    if let error = error {
+                        print(error)
+                    }
+                    else {
+                        print("loginWithKakaoTalk() success.")
 
+                        //do something
+                        _ = oauthToken
+                    }
+            }
+        }
+    }
+    
 }
 
