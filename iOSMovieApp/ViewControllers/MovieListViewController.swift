5f46a4cd988bb88376b906b813d881936fffaa6e
h0225w <gusdn5387@naver.com>
2022-06-17 22:26:10 +0900

    영화 목록 > 검색 기능 추가

--- iOSMovieApp/ViewControllers/MovieListViewController.swift
+++ iOSMovieApp/ViewControllers/MovieListViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 class MovieListViewController: UIViewController {
     let movieListService = MovieList()
     
+    @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var tableView: UITableView!
     
     var movieList: [Item] = []
@@ -18,19 +19,36 @@ class MovieListViewController: UIViewController {
         super.viewDidLoad()
         
         setupTableView()
-        movieListService.getMovieList(nil) { data in
-            self.movieList = data
-            self.tableView.reloadData()
-        }
+        setupSearchBar()
+        search("")
     }
 }
 
 // MARK: - Extension
 private extension MovieListViewController {
+    // MARK: TableView 설정
     func setupTableView() {
         tableView.delegate = self
         tableView.dataSource = self
         tableView.register(UINib(nibName: "MovieListTableViewCell", bundle: nil), forCellReuseIdentifier: "MovieListCell")
+    }
+    
+    // MARK: SearchBar 설정
+    func setupSearchBar() {
+        searchBar.delegate = self
+    }
+    
+    // MARK: 영화 목록 > 검색 기능
+    func search(_ query: String?) {
+        movieListService.getMovieList(Search(query: query, genre: 0)) { data in
+            self.movieList = data
+            self.tableView.reloadData()
+        }
+    }
+    
+    // MARK: 검색 후 키보드 내려가도록
+    func dismissKeyboard() {
+        searchBar.resignFirstResponder() // 첫 번째 응답자 사임
     }
 }
 
@@ -53,4 +71,15 @@ extension MovieListViewController: UITableViewDelegate, UITableViewDataSource {
         return 200.0
     }
     
+}
+
+
+// MARK: - UISearchBarDelegate
+extension MovieListViewController: UISearchControllerDelegate, UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        guard let query = searchBar.text, query.isEmpty == false else { return }
+        
+        dismissKeyboard()
+        search(query)
+    }
 }
